d7fecdae35dc848b8376b31fccb3b63a3a81cf28
Cxxxa <cinnaxinali@outlook.com>
2019-11-22 11:20:52 +0800

    Check path and fix push problem

--- trial.R
+++ trial.R
@@ -1,9 +1,14 @@
 # R code 
 
+# Tools - Shell
+# git config --global user.name 'Cxxxa'
+# git config --global user.email 'cinnaxinali@outlook.com'
+
+
+# Commit:
 # In RStudio press the Git icon and select Commit.. from Git menu (Ctrl+Alt+M) to open the commit window
 
-# Try to push
-
+# Push: 
 # if fail to push:
 # in RStudio, click on the "Tools" menu and select "Shell"
 # Run the following command: git push -u origin master
