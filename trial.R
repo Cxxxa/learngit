bc4638d85198d41136214fed3029d9750aebd381
Cxxxa <cinnaxinali@outlook.com>
2019-11-22 11:15:16 +0800

    second trial

--- trial.R
+++ trial.R
@@ -3,3 +3,7 @@
 # In RStudio press the Git icon and select Commit.. from Git menu (Ctrl+Alt+M) to open the commit window
 
 # Try to push
+
+# if fail to push:
+# in RStudio, click on the "Tools" menu and select "Shell"
+# Run the following command: git push -u origin master
